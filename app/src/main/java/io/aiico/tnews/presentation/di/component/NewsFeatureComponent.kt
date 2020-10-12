99066549964d84af30c7802b4ffe7211d3a60218
Alexander Ischuk <27893510+isalig@users.noreply.github.com>
2020-10-13 01:00:51 +0300

    Clean domain layer (#6)

--- app/src/main/java/io/aiico/tnews/presentation/di/component/NewsFeatureComponent.kt
+++ app/src/main/java/io/aiico/tnews/presentation/di/component/NewsFeatureComponent.kt
@@ -3,9 +3,10 @@ package io.aiico.tnews.presentation.di.component
 import dagger.Component
 import io.aiico.tnews.presentation.MainActivity
 import io.aiico.tnews.presentation.di.CommonDependencies
+import io.aiico.tnews.presentation.di.module.DataModule
 import io.aiico.tnews.presentation.list.NewsTitlesFragment
 
-@Component(dependencies = [CommonDependencies::class])
+@Component(dependencies = [CommonDependencies::class], modules = [DataModule::class])
 interface NewsFeatureComponent : CommonDependencies {
 
     fun inject(activity: MainActivity)
