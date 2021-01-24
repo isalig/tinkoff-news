651d95fb11031e057e4a961b1dc08baa41bb7bee
isalig <ischuk.aleksander@gmail.com>
2021-01-25 00:36:40 +0300

    Add buildSrc

---
+++ buildSrc/src/main/kotlin/Dependencies.kt
@@ -0,0 +1,23 @@
+object Dependencies {
+  const val kotlin = "org.jetbrains.kotlin:kotlin-stdlib:${Versions.kotlin}"
+
+  const val dagger = "com.google.dagger:dagger:${Versions.dagger}"
+  const val daggerCompiler = "com.google.dagger:dagger-compiler:${Versions.dagger}"
+
+  const val rxJava = "io.reactivex.rxjava2:rxjava:${Versions.rxJava}"
+  const val rxAndroid = "io.reactivex.rxjava2:rxandroid:${Versions.rxAndroid}"
+
+  const val retrofit =  "com.squareup.retrofit2:retrofit:${Versions.retrofit}}"
+  const val retrofitRxAdapter = "com.squareup.retrofit2:adapter-rxjava2:${Versions.retrofit}"
+  const val retrofitGsonConverter = "com.squareup.retrofit2:converter-gson:${Versions.retrofit}"
+
+  const val appCompat = "androidx.appcompat:appcompat:${Versions.appCompat}"
+  const val material = "com.google.android.material:material:${Versions.material}"
+  const val swipeRefreshLayout = "androidx.swiperefreshlayout:swiperefreshlayout:${Versions.swipeRefresh}"
+  const val recyclerView = "androidx.recyclerview:recyclerview:${Versions.recyclerView}"
+
+  const val junit = "junit:junit:${Versions.junit}"
+  const val mockitoKotlin = "com.nhaarman.mockitokotlin2:mockito-kotlin:${Versions.mockito}"
+
+  const val ktxCore = "androidx.core:core-ktx:${Versions.ktxCore}"
+}
---
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -0,0 +1,27 @@
+object Versions {
+  const val kotlin = "1.4.21"
+  const val java = 1.8
+
+  const val compileSdk = 30
+  const val buildTools = "29.0.3"
+
+  const val minSdk = 21
+  const val targetSdk = 30
+  const val versionCode = 1
+  const val versionName = "1.0"
+
+  const val rxJava = "2.2.19"
+  const val rxAndroid = "2.1.1"
+  const val dagger = "2.28.3"
+  const val retrofit = "2.8.1"
+
+  const val appCompat = "1.2.0"
+  const val material = "1.2.1"
+  const val swipeRefresh = "1.1.0"
+  const val recyclerView = "1.1.0"
+
+  const val ktxCore = "1.3.2"
+
+  const val junit = "4.13"
+  const val mockito = "2.2.0"
+}
